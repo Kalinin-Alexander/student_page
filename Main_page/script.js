f46dcab21f2fa944fe772291b0cddb2058c7a096
Kalinin-Alexander <128771078+Kalinin-Alexander@users.noreply.github.com>
2023-10-06 08:35:33 +0300

    Add files via upload

---
+++ Main_page/script.js
@@ -0,0 +1,19 @@
+let modal = document.getElementById("modal");
+
+let btn = document.getElementById("contact_btn");
+
+let span = document.getElementsByClassName("modal_close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
