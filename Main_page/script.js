90ab3ccc5e7e8d06c7a772cb079f82066f32f852
Kalinin-Alexander <128771078+Kalinin-Alexander@users.noreply.github.com>
2023-11-30 10:56:28 +0300

    Update script.js

--- Main_page/script.js
+++ Main_page/script.js
@@ -14,16 +14,6 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-/*Не срабатывает вместе с другим window.onclick*/
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-
 /* Для Dropdown*/
 
 /* задание переменных*/ 
@@ -40,91 +30,36 @@ let drop4 = document.getElementById("news");
 let call4 = document.getElementById("news_call");
 
 
+function openDropdown(call, drop){
+  call.onclick = () =>{
+    if (drop.style.display == "block"){
+      drop.style.display = "none";
+    }
+    else{
+      drop.style.display = "block";
+    }
+  }
+}
 
-/* функции*/
-function openDropdown(drop){
-  if (drop.style.display == "block"){
+function closeDropdown(event, drop, call, id){
+  if (event.target !== drop && event.target !== call && !event.target.closest(id) && drop.style.display === "block"){
     drop.style.display = "none";
   }
-  else{
-    drop.style.display = "block";
-  }
 }
 
 
-function closeDropdown(drop){
-  
-}
+openDropdown(call1, drop1);
+openDropdown(call2, drop2);
+openDropdown(call3, drop3);
+openDropdown(call4, drop4);
 
 
-/* Перенести в функцию closeDropdown*/
 window.onclick = function(event) {
-  if (event.target !== drop1 && event.target !== call1 && !event.target.closest('#student') && drop1.style.display === "block") {
-    drop1.style.display = "none";
-  }
-  if (event.target !== drop2 && event.target !== call2 && !event.target.closest('#learn') && drop2.style.display === "block") {
-    drop2.style.display = "none";
-  }
-  if (event.target !== drop3 && event.target !== call3 && !event.target.closest('#work') && drop3.style.display === "block") {
-    drop3.style.display = "none";
-  }
-  if (event.target !== drop4 && event.target !== call4 && !event.target.closest('#news') && drop4.style.display === "block") {
-    drop4.style.display = "none";
+  closeDropdown(event, drop1, call1, "#student");
+  closeDropdown(event, drop2, call2, "#learn");
+  closeDropdown(event, drop3, call3, "#work");
+  closeDropdown(event, drop4, call4, "#news");
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
-
-
-/* Перенесено в функцию openDropdown
-
-call1.onclick = function() {
-  if (drop1.style.display == "block"){
-    drop1.style.display = "none";
-  }
-  else{
-    drop1.style.display = "block";
-  }
-  drop4.style.display = "none";
-  drop3.style.display = "none";
-  drop2.style.display = "none";
-}
-
-
-call2.onclick = function() {
-  if (drop2.style.display == "block"){
-    drop2.style.display = "none";
-  }
-  else{
-    drop2.style.display = "block";
-  }
-  drop4.style.display = "none";
-  drop3.style.display = "none";
-  drop1.style.display = "none";
-}
-
-
-call3.onclick = function() {
-  if (drop3.style.display == "block"){
-    drop3.style.display = "none";
-  }
-  else{
-    drop3.style.display = "block";
-  }
-  drop4.style.display = "none";
-  drop2.style.display = "none";
-  drop1.style.display = "none";
-}
-
-
-call4.onclick = function() {
-  if (drop4.style.display == "block"){
-    drop4.style.display = "none";
-  }
-  else{
-    drop4.style.display = "block";
-  }
-  drop3.style.display = "none";
-  drop2.style.display = "none";
-  drop1.style.display = "none";
-}
-
-*/
